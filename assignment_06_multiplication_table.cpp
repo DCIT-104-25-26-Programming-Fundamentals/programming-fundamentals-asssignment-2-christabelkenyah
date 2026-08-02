8e2bd1e8ada92dd5a243d9a07ceb3563a53cca6b
christabelkenyah <christabelkenyah60@gmail.com>
2026-08-02 22:24:06 +0000

    Add Fibonacci sequence and multiplication table functions

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -56,4 +56,93 @@
 
 #include <iostream>
 using namespace std;
+void printFibonacci(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    int a = 0, b = 1;
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+}
+
+bool isFibonacci(int num) {
+    if (num < 0) {
+        return false;
+    }
+
+    int a = 0, b = 1;
+    while (a <= num) {
+        if (a == num) {
+            return true;
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cout << "How many terms? ";
+    cin >> n;
+    printFibonacci(n);
+
+    int num;
+    cout << "\nEnter a number to check: ";
+    cin >> num;
+
+    if (isFibonacci(num)) {
+        cout << num << " is a Fibonacci number." << endl;
+    } else {
+        cout << num << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
+5
+void printTable(int num) {
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << num << " x " << i << " = " << (num * i) << endl;
+    }
+}
+
+void printTablesUpToN(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        printTable(num);
+        if (num != n) {
+            cout << "---" << endl;
+        }
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    printTable(number);
+
+    cout << endl;
+
+    int n;
+    cout << "Enter a number N: ";
+    cin >> n;
+    printTablesUpToN(n);
+
+    return 0;
+
 
